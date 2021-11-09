b2d1abddc583f340c5f9556481c5557274e61284
m78355ls <luke.sanderson@student.manchester.ac.uk>
2021-11-09 18:22:19 +0000

    removing unused i variable. Making it output in lowercase. Remove uneeded topdown variable thing in output

--- midterm_files/decrypt_m78355ls.py
+++ midterm_files/decrypt_m78355ls.py
@@ -35,16 +35,12 @@ def decrypt(inputString):
     else:
         outputString = decryptMorse(inputString)
 
-    print(outputString)
-    return outputString
+    return outputString.lower()
 
-for root, dirs, files in os.walk(sys.argv[1], topdown=True):
-    i=1
-    print(files)
+for root, dirs, files in os.walk(sys.argv[1]):
     for file in files:
         with open(root + file) as inputFile:
             outputString = decrypt(inputFile.read())
 
         with open(sys.argv[2]+file[:-4]+"_m78355ls.txt","w") as outputFile:
             outputFile.write(outputString)
-        i += 1
