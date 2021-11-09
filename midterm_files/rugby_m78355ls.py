5451832f86b1b60399731dce297fb0fcdf7d20ed
m78355ls <luke.sanderson@student.manchester.ac.uk>
2021-11-09 16:45:33 +0000

    changed to work with folder inputs

--- midterm_files/rugby_m78355ls.py
+++ midterm_files/rugby_m78355ls.py
@@ -1,41 +1,50 @@
 import sys
+import os
 #python3 rugby (student number).py [input file path] [output file path]
+def calcScore(inputString):
+    score1 = 0
+    score2 = 0
 
-with open(sys.argv[1]) as inputFile:
-    inputString = inputFile.read()
+    for char in inputString:
 
-score1 = 0
-score2 = 0
+        if char == 't':
+            if prev == '1':
+                score1 += 5
+            else: score2 += 5
+        if char == 'c':
+            if prev == '1':
+                score1 += 2
+            else: score2 += 2
+        if char == 'p':
+            if prev == '1':
+                score1 += 3
+            else: score2 += 3
+        if char == 'd':
+            if prev == '1':
+                score1 += 1
+            else: score2 += 1
 
-for char in inputString:
+        prev = char
 
-    if char == 't':
-        if prev == '1':
-            score1 += 5
-        else: score2 += 5
-    if char == 'c':
-        if prev == '1':
-            score1 += 2
-        else: score2 += 2
-    if char == 'p':
-        if prev == '1':
-            score1 += 3
-        else: score2 += 3
-    if char == 'd':
-        if prev == '1':
-            score1 += 1
-        else: score2 += 1
+    if score1 > score2:
+        print("T1 wins!")
+    elif score1 < score2:
+        print("T2 wins!")
+    else:
+        print("Its a draw!")
 
-    prev = char
+    outputString = str(score1)+":"+str(score2)
+    print(outputString)
 
-if score1 > score2:
-    print("T1 wins!")
-elif score1 < score2:
-    print("T2 wins!")
-else:
-    print("Its a draw!")
+    return outputString
 
-outputString = str(score1)+":"+str(score2)
-print(outputString)
-with open(sys.argv[2],"w") as outputFile:
-    outputFile.write(outputString)
+
+for root, dirs, files in os.walk(sys.argv[1]):
+    i=1
+    for file in files:
+        with open(root + file) as inputFile:
+            outputString = calcScore(inputFile.read())
+
+        with open(sys.argv[2]+"test_file"+str(i)+"_m78355ls.txt","w") as outputFile:
+            outputFile.write(outputString)
+        i += 1
