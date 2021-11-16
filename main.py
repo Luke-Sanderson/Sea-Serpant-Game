fa5550c425921eff17f213530a96ba18572f4218
m78355ls <luke.sanderson@student.manchester.ac.uk>
2021-11-16 23:02:51 +0000

    Made segments follow each other and added water level

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 #Resolution set to 1366x768
 import tkinter as tk
-from math import sin, cos, atan, pi
+from math import sin, cos, atan2, pi, sqrt
 from time import perf_counter_ns
 
 class Segment:
     """docstring for Segment."""
-    radius = 33
-    # x_velocity = 0
-    # y_velocity = 0
-    velocity = 2
-    direction = 0
-    def __init__(self, x, y, canvas):
+    radius = 50
+    acceleration = 1
+    max_velocity = 8
+    velocity = 0
+    x_velocity = 0
+    y_velocity = 0
 
+    direction = 0
+    GRAVITY = 0.1
+
+    def __init__(self, x, y, canvas):
         self.drawing = canvas.create_oval(x, y, self.radius + x, self.radius + y, fill="blue")
         self.canvas = canvas
-        #self.canvas.bind("<Button-1>",self.centre)
+
     def update(self):
-        x_velocity = cos(self.direction) * self.velocity
-        y_velocity = sin(self.direction) * self.velocity
-        self.canvas.move(self.drawing, x_velocity, y_velocity)
+        if self.canvas.coords(self.drawing)[1] > 200:
+            self.velocity += self.acceleration
+            if self.velocity > self.max_velocity: self.velocity = self.max_velocity
+            self.x_velocity = cos(self.direction) * self.velocity
+            self.y_velocity = sin(self.direction) * self.velocity
+        else:
+           self.y_velocity += self.GRAVITY
+
+        # print("")
+        # print(self.direction)
+        # print(self.x_velocity)
+        # print(self.y_velocity)
+        self.canvas.move(self.drawing, self.x_velocity, self.y_velocity)
 
 class Game:
     """docstring for Game."""
@@ -35,9 +49,14 @@ class Game:
         self.canvas.pack()
 
         self.head = Segment(100,100,self.canvas)
+        self.head.head = 1
+        self.canvas.itemconfig(self.head.drawing, fill="green")
         self.serpent.append(self.head)
-        self.serpent.append(Segment(100 - 50 * len(self.serpent),100,self.canvas))
+        for i in range(5):
+            self.serpent.append(Segment(100 - 20 * len(self.serpent),100,self.canvas))
+        #self.serpent.append(Segment(100 - 50 * len(self.serpent),100,self.canvas))
 
+        self.sea = self.canvas.create_line(0,200,1366,200, fill="Blue")
 
         self.previous_time = perf_counter_ns()
 
@@ -49,8 +68,10 @@ class Game:
     def key_press(self, Key):
         if(Key.keysym.lower() == "a"): #Keep to lower
             self.head.direction -= 0.1
+            self.head.velocity -= 0.1
         elif(Key.keysym == "d"):
             self.head.direction += 0.1
+            self.head.velocity -= 0.1
         elif(Key.keysym == "Escape"):
             exit()
 
@@ -61,14 +82,20 @@ class Game:
             if index != 0:
                 prev = self.serpent[index - 1].canvas.coords(self.serpent[index - 1].drawing)
                 cur = segment.canvas.coords(segment.drawing)
+                vector = (prev[0] - cur[0], prev[1] - cur[1])
                 x_change = prev[0] - cur[0]
-                if x_change != 0:
-                    new_direction = atan((prev[1] - cur[1])/(prev[0] - cur[0]))
-                    if new_direction < 0:
-                        new_direction += 2*pi
-                    segment.direction = new_direction
+                new_direction = atan2(vector[1], vector[0])
+                segment.direction = new_direction
 
-        
+                #Adjust speed
+                distance = sqrt(vector[0]**2 + vector[1]**2)
+                if distance < 30 and segment.max_velocity > 0:
+                    segment.max_velocity -= 0.05
+                elif distance > 20:
+                    segment.max_velocity += 0.05
+#                print(segment.velocity)
+
+
             segment.update()
 
             prev = segment.canvas.coords(segment.drawing)
@@ -77,17 +104,13 @@ class Game:
     def game_loop(self):
 
         #Calculates difference in time between update loops and adjusts tick rate
-        #print((perf_counter_ns() - self.previous_time)/1000000)
         difference = (100/3) - ((perf_counter_ns() - self.previous_time)/1000000)
         self.tick_rate += round(difference)
         self.previous_time = perf_counter_ns()
-
         self.window.after(self.tick_rate, self.game_loop)
 
         self.move_serpent()
 
-        #self.canvas.delete("all")
-        #self.canvas.move(self.head.drawing,10,10)
 
 #    def draw():
 
