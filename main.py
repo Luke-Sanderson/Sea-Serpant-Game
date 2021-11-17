891a0928266eb7041e91190182a3f87d8b8779ea
m78355ls <luke.sanderson@student.manchester.ac.uk>
2021-11-17 17:07:46 +0000

    Fixed gravity when jumping out of water. Also made segments more stable by changing max velocity to constant rather than adjusting cumulatively

--- main.py
+++ main.py
@@ -13,25 +13,26 @@ class Segment:
     y_velocity = 0
 
     direction = 0
-    GRAVITY = 0.1
+    GRAVITY = 0.2
 
-    def __init__(self, x, y, canvas):
+    def __init__(self, x, y, canvas, id):
         self.drawing = canvas.create_oval(x, y, self.radius + x, self.radius + y, fill="blue")
         self.canvas = canvas
-
+        self.id = id
     def update(self):
-        if self.canvas.coords(self.drawing)[1] > 200:
+        if self.canvas.coords(self.drawing)[1] > 200 or self.id != 0:
             self.velocity += self.acceleration
             if self.velocity > self.max_velocity: self.velocity = self.max_velocity
             self.x_velocity = cos(self.direction) * self.velocity
             self.y_velocity = sin(self.direction) * self.velocity
         else:
            self.y_velocity += self.GRAVITY
+           self.velocity = self.y_velocity
+           if self.direction > pi:
+               self.direction = 2*pi -self.direction
+           elif self.direction < 0:
+               self.direction = -self.direction
 
-        # print("")
-        # print(self.direction)
-        # print(self.x_velocity)
-        # print(self.y_velocity)
         self.canvas.move(self.drawing, self.x_velocity, self.y_velocity)
 
 class Game:
@@ -40,20 +41,21 @@ class Game:
     tick_rate = 50
 
     def __init__(self):
-        self.window = tk.Tk()
-        self.window.title("Sea Serpant Game")
-        self.window.geometry("1366x768")
-        self.window.attributes("-fullscreen", True)
+        self.root = tk.Tk()
+        self.root.title("Sea Serpant Game")
+        self.root.geometry("1366x768")
+        self.root.attributes("-fullscreen", True)
+        self.root.config(cursor = "man")
 
-        self.canvas = tk.Canvas(self.window, width="1366", height="768")
+        self.canvas = tk.Canvas(self.root, width="1366", height="768")
         self.canvas.pack()
 
-        self.head = Segment(100,100,self.canvas)
+        self.head = Segment(100,400,self.canvas, 0)
         self.head.head = 1
         self.canvas.itemconfig(self.head.drawing, fill="green")
         self.serpent.append(self.head)
-        for i in range(5):
-            self.serpent.append(Segment(100 - 20 * len(self.serpent),100,self.canvas))
+        for i in range(10):
+            self.serpent.append(Segment(100 - 20 * len(self.serpent),400,self.canvas, i + 1))
         #self.serpent.append(Segment(100 - 50 * len(self.serpent),100,self.canvas))
 
         self.sea = self.canvas.create_line(0,200,1366,200, fill="Blue")
@@ -61,19 +63,23 @@ class Game:
         self.previous_time = perf_counter_ns()
 
     def main_loop(self):
-        self.window.after(self.tick_rate, self.game_loop)
-        self.window.bind("<Key>", self.key_press)
-        self.window.mainloop()
+        self.root.after(self.tick_rate, self.game_loop)
+        self.root.bind("<Key>", self.key_press)
+        self.root.mainloop()
 
     def key_press(self, Key):
-        if(Key.keysym.lower() == "a"): #Keep to lower
+
+        if(Key.keysym.lower() == "a" and self.head.canvas.coords(self.head.drawing)[1] > 200):
             self.head.direction -= 0.1
             self.head.velocity -= 0.1
-        elif(Key.keysym == "d"):
+        elif(Key.keysym.lower() == "d" and self.head.canvas.coords(self.head.drawing)[1] > 200):
             self.head.direction += 0.1
             self.head.velocity -= 0.1
         elif(Key.keysym == "Escape"):
             exit()
+        elif(Key.keysym.lower() == "c"):
+            coords = self.canvas.coords(self.serpent[len(self.serpent) - 1].drawing)
+            self.serpent.append(Segment(coords[0],coords[1],self.canvas, len(self.serpent)))
 
     def move_serpent(self):
 
@@ -89,12 +95,16 @@ class Game:
 
                 #Adjust speed
                 distance = sqrt(vector[0]**2 + vector[1]**2)
-                if distance < 30 and segment.max_velocity > 0:
-                    segment.max_velocity -= 0.05
-                elif distance > 20:
-                    segment.max_velocity += 0.05
-#                print(segment.velocity)
+                if distance > 30:
+                    segment.max_velocity =  9 #Max Vel at 8
+                elif distance < 20:
+                    segment.max_velocity = 6
+                elif distance < 5:
+                    segment.max_velocity = 1
+                else:
+                    segment.max_velocity = 8
 
+#                print(segment.velocity)
 
             segment.update()
 
@@ -107,7 +117,7 @@ class Game:
         difference = (100/3) - ((perf_counter_ns() - self.previous_time)/1000000)
         self.tick_rate += round(difference)
         self.previous_time = perf_counter_ns()
-        self.window.after(self.tick_rate, self.game_loop)
+        self.root.after(self.tick_rate, self.game_loop)
 
         self.move_serpent()
 
