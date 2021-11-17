f1411f7905302b4ff20864bd3f9f4f1c2993a86a
m78355ls <luke.sanderson@student.manchester.ac.uk>
2021-11-17 21:39:37 +0000

    Added basic menu system

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #Resolution set to 1366x768
-from tkinter import Tk, Canvas
+from tkinter import Tk, Canvas, ttk
 from time import perf_counter_ns
 from serpent import *
 
 class Game:
     """docstring for Game."""
-    segment_array = []
     tick_rate = 30
+    GAME_STATES = ["main_menu", "pause", "in_game"]
 
     def __init__(self):
         self.root = Tk()
@@ -15,19 +15,57 @@ class Game:
         self.root.attributes("-fullscreen", True)
         self.root.config(cursor = "pirate")
 
-        self.canvas = Canvas(self.root, width="1366", height="768")
-        self.canvas.pack()
+        self.current_game_state = self.GAME_STATES[0]
 
-        self.head = Segment(100,400,self.canvas, 0)
-        self.canvas.itemconfig(self.head.drawing, fill="green")
-        self.segment_array.append(self.head)
-        for i in range(10):
-            self.segment_array.append(Segment(100 - 20 * len(self.segment_array),400,self.canvas, i + 1))
-        #self.segment_array.append(Segment(100 - 50 * len(self.segment_array),100,self.canvas))
+        self.canvas_game = Canvas(self.root, width="1366", height="768")
+        self.canvas_menu = Canvas(self.root, width="1366", height="768")
 
-        self.sea = self.canvas.create_line(0,200,1366,200, fill="Blue")
+        self.initialise_menu()
+        self.canvas_menu.pack()
 
         self.previous_time = perf_counter_ns()
+
+    def switch_scene(self, next):
+
+        if self.current_game_state == "main_menu":
+            if next == "in_game":
+                self.canvas_menu.pack_forget()
+                self.canvas_game.delete("all")
+                self.initialise_game()
+                self.canvas_game.pack()
+                self.root.after(self.tick_rate, self.game_loop)
+
+        elif self.current_game_state == "in_game":
+            if next == "main_menu":
+                self.canvas_game.pack_forget()
+                self.canvas_menu.pack()
+
+        self.current_game_state = next
+
+    def initialise_menu(self):
+        self.menu_buttons = []
+        self.menu_buttons.append(ttk.Button(self.canvas_menu, text = "Play game", command=lambda: self.switch_scene(self.GAME_STATES[2])))
+        self.menu_buttons.append(ttk.Button(self.canvas_menu, text = "Settings"))
+        self.menu_buttons.append(ttk.Button(self.canvas_menu, text = "Quit", command=exit))
+
+        for index, button in enumerate(self.menu_buttons):
+            y= 273 + 60*index
+            button.place(x="533",y=str(y),width="300", height="50")
+
+
+    def initialise_game(self):
+        self.segment_array = []
+
+        self.head = Segment(100,400,self.canvas_game, 0)
+        self.canvas_game.itemconfig(self.head.drawing, fill="green")
+        self.segment_array.append(self.head)
+
+
+        for i in range(10):
+            self.segment_array.append(Segment(100 - 20 * len(self.segment_array),400,self.canvas_game, i + 1))
+            self.head.canvas.lift(self.head.drawing) #Optional make head pop
+
+        self.sea = self.canvas_game.create_line(0,200,1366,200, fill="Blue")
 
     def main_loop(self):
         self.root.after(self.tick_rate, self.game_loop)
@@ -43,10 +81,10 @@ class Game:
             self.head.direction += 0.1
             self.head.velocity -= 0.1
         elif(Key.keysym == "Escape"):
-            exit()
+            self.switch_scene(self.GAME_STATES[0])
         elif(Key.keysym.lower() == "c"):
-            coords = self.canvas.coords(self.segment_array[len(self.segment_array) - 1].drawing)
-            self.segment_array.append(Segment(coords[0],coords[1],self.canvas, len(self.segment_array)))
+            coords = self.canvas_game.coords(self.segment_array[len(self.segment_array) - 1].drawing)
+            self.segment_array.append(Segment(coords[0],coords[1],self.canvas_game, len(self.segment_array)))
 
 
 
@@ -56,10 +94,10 @@ class Game:
         difference = (100/3) - ((perf_counter_ns() - self.previous_time)/1000000)
         self.tick_rate += round(difference)
         self.previous_time = perf_counter_ns()
-        self.root.after(self.tick_rate, self.game_loop)
-
-        for segment in self.segment_array:
-            segment.move_serpent(self.segment_array)
+        if self.current_game_state == self.GAME_STATES[2]:
+            for segment in self.segment_array:
+                segment.move_serpent(self.segment_array)
+            self.root.after(self.tick_rate, self.game_loop)
 
 
 
