c9373108c164c83d7e762f9c503a4e5ccdeaf581
m78355ls <luke.sanderson@student.manchester.ac.uk>
2021-11-17 19:47:05 +0000

    Seperated serpent logic into its own file. Also added collision between serpent and screen edge

--- main.py
+++ main.py
@@ -1,62 +1,29 @@
 #Resolution set to 1366x768
-import tkinter as tk
-from math import sin, cos, atan2, pi, sqrt
+from tkinter import Tk, Canvas
 from time import perf_counter_ns
-
-class Segment:
-    """docstring for Segment."""
-    radius = 50
-    acceleration = 1
-    max_velocity = 8
-    velocity = 0
-    x_velocity = 0
-    y_velocity = 0
-
-    direction = 0
-    GRAVITY = 0.2
-
-    def __init__(self, x, y, canvas, id):
-        self.drawing = canvas.create_oval(x, y, self.radius + x, self.radius + y, fill="blue")
-        self.canvas = canvas
-        self.id = id
-    def update(self):
-        if self.canvas.coords(self.drawing)[1] > 200 or self.id != 0:
-            self.velocity += self.acceleration
-            if self.velocity > self.max_velocity: self.velocity = self.max_velocity
-            self.x_velocity = cos(self.direction) * self.velocity
-            self.y_velocity = sin(self.direction) * self.velocity
-        else:
-           self.y_velocity += self.GRAVITY
-           self.velocity = self.y_velocity
-           if self.direction > pi:
-               self.direction = 2*pi -self.direction
-           elif self.direction < 0:
-               self.direction = -self.direction
-
-        self.canvas.move(self.drawing, self.x_velocity, self.y_velocity)
+from serpent import *
 
 class Game:
     """docstring for Game."""
-    serpent = []
-    tick_rate = 50
+    segment_array = []
+    tick_rate = 30
 
     def __init__(self):
-        self.root = tk.Tk()
+        self.root = Tk()
         self.root.title("Sea Serpant Game")
         self.root.geometry("1366x768")
         self.root.attributes("-fullscreen", True)
-        self.root.config(cursor = "man")
+        self.root.config(cursor = "pirate")
 
-        self.canvas = tk.Canvas(self.root, width="1366", height="768")
+        self.canvas = Canvas(self.root, width="1366", height="768")
         self.canvas.pack()
 
         self.head = Segment(100,400,self.canvas, 0)
-        self.head.head = 1
         self.canvas.itemconfig(self.head.drawing, fill="green")
-        self.serpent.append(self.head)
+        self.segment_array.append(self.head)
         for i in range(10):
-            self.serpent.append(Segment(100 - 20 * len(self.serpent),400,self.canvas, i + 1))
-        #self.serpent.append(Segment(100 - 50 * len(self.serpent),100,self.canvas))
+            self.segment_array.append(Segment(100 - 20 * len(self.segment_array),400,self.canvas, i + 1))
+        #self.segment_array.append(Segment(100 - 50 * len(self.segment_array),100,self.canvas))
 
         self.sea = self.canvas.create_line(0,200,1366,200, fill="Blue")
 
@@ -78,37 +45,9 @@ class Game:
         elif(Key.keysym == "Escape"):
             exit()
         elif(Key.keysym.lower() == "c"):
-            coords = self.canvas.coords(self.serpent[len(self.serpent) - 1].drawing)
-            self.serpent.append(Segment(coords[0],coords[1],self.canvas, len(self.serpent)))
+            coords = self.canvas.coords(self.segment_array[len(self.segment_array) - 1].drawing)
+            self.segment_array.append(Segment(coords[0],coords[1],self.canvas, len(self.segment_array)))
 
-    def move_serpent(self):
-
-        for index, segment in enumerate(self.serpent):
-
-            if index != 0:
-                prev = self.serpent[index - 1].canvas.coords(self.serpent[index - 1].drawing)
-                cur = segment.canvas.coords(segment.drawing)
-                vector = (prev[0] - cur[0], prev[1] - cur[1])
-                x_change = prev[0] - cur[0]
-                new_direction = atan2(vector[1], vector[0])
-                segment.direction = new_direction
-
-                #Adjust speed
-                distance = sqrt(vector[0]**2 + vector[1]**2)
-                if distance > 30:
-                    segment.max_velocity =  9 #Max Vel at 8
-                elif distance < 20:
-                    segment.max_velocity = 6
-                elif distance < 5:
-                    segment.max_velocity = 1
-                else:
-                    segment.max_velocity = 8
-
-#                print(segment.velocity)
-
-            segment.update()
-
-            prev = segment.canvas.coords(segment.drawing)
 
 
     def game_loop(self):
@@ -119,7 +58,9 @@ class Game:
         self.previous_time = perf_counter_ns()
         self.root.after(self.tick_rate, self.game_loop)
 
-        self.move_serpent()
+        for segment in self.segment_array:
+            segment.move_serpent(self.segment_array)
+
 
 
 #    def draw():
---
+++ serpent.py
@@ -0,0 +1,70 @@
+from math import pi, sin, cos, atan2, sqrt
+
+class Segment:
+    """docstring for Segment."""
+    radius = 50
+    acceleration = 1
+    max_velocity = 8
+    velocity = 0
+    x_velocity = 0
+    y_velocity = 0
+
+    direction = 0
+    GRAVITY = 0.15
+
+    def __init__(self, x, y, canvas, id):
+        self.drawing = canvas.create_oval(x, y, self.radius + x, self.radius + y, fill="blue")
+        self.canvas = canvas
+        self.id = id
+
+    def move_serpent(self, segment_array):
+        current_coords = self.canvas.coords(self.drawing)
+
+        if current_coords[0] < 0:
+            self.canvas.coords(self.drawing, 0, current_coords[1], self.radius, current_coords[1] + self.radius)
+            self.direction = 0
+        elif current_coords[0] + self.radius > 1366:
+            self.canvas.coords(self.drawing, 1366 - self.radius, current_coords[1], 1366, current_coords[1] + self.radius)
+            self.direction = pi
+        elif current_coords[1] + self.radius > 768:
+            self.canvas.coords(self.drawing, current_coords[0], 768 - self.radius, current_coords[0] + self.radius, 768)
+            self.direction = 1.5 * pi
+
+        if self.id != 0:
+            prev = segment_array[self.id - 1].canvas.coords(segment_array[self.id - 1].drawing)
+            vector = (prev[0] - current_coords[0], prev[1] - current_coords[1])
+            x_change = prev[0] - current_coords[0]
+            new_direction = atan2(vector[1], vector[0])
+            self.direction = new_direction
+
+            #Adjust speed
+            distance = sqrt(vector[0]**2 + vector[1]**2)
+            if distance > 30:
+                self.max_velocity =  9 #Max Vel at 8
+            elif distance < 4:
+                self.max_velocity = 0
+            elif distance < 10:
+                self.max_velocity = 3
+            elif distance < 20:
+                self.max_velocity = 6
+            else:
+                self.max_velocity = 8
+
+    #                print(segment.velocity)
+        if current_coords[1] > 200 or self.id != 0:
+            self.velocity += self.acceleration
+            if self.velocity > self.max_velocity: self.velocity = self.max_velocity
+            self.x_velocity = cos(self.direction) * self.velocity
+            self.y_velocity = sin(self.direction) * self.velocity
+        else:
+           self.y_velocity += self.GRAVITY
+           self.velocity = self.y_velocity
+           if self.direction > pi:
+               self.direction = 2*pi -self.direction
+           elif self.direction < 0:
+               self.direction = -self.direction
+
+
+
+
+        self.canvas.move(self.drawing, self.x_velocity, self.y_velocity)
